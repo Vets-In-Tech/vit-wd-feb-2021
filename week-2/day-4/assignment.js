01023875020b860a18ceb01f141981ca076b4689
Stephanie Lentell <slentell@aol.com>
2021-03-08 12:20:25 -0500

    Week 2 Assignment Completed

--- week-2/day-4/assignment.js
+++ week-2/day-4/assignment.js
@@ -1,16 +1,16 @@
-var imgIndex = 1
-showImg(imgIndex);
+var counter = 0;
 
 function moveBack() {
-    var carouse = document.getElementById("imgSlider");
-    var previous = document.getElementById("previous");
-    currentImg = previous -1;
-  
-  
-}
+    if (counter > 0) {
+      counter --;
+      document.getElementById("imgSlider2").style.transform='translateX(-'+253*counter+'px)';
+      console.log(counter);
+    }
+  }
 function moveForward() {
-    var carousel = document.getElementById("imgSlider");
-  var next = document.getElementById("next");
-  next = currentImg +1;
-  carousel[currentImg].className = "img shown"
-}
+  if (counter < 13) {
+    counter ++;
+    document.getElementById("imgSlider2").style.transform='translateX(-'+253*counter+'px)';
+    console.log(counter);
+    }
+  }
