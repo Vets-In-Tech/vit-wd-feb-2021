b0f10b561502dfcc3ea8ca3d35d02ca395827899
Robert Garcia <robbie559@gmail.com>
2021-03-08 04:08:14 -0800

    Pushing Assigntment 2

---
+++ week-2/day-4/script.js
@@ -0,0 +1,12 @@
+// Script for Carousel
+var title = document.getElementById('grabMe');
+
+function leftclickCarousel(){
+    
+    document.getElementById("myDIV").style.transform = "TranslateX(500px)";
+}
+
+function rightclickCarousel(){
+    
+    document.getElementById("myDIV").style.transform = "TranslateX(-500px)";
+}
