5ac1a3bb388f436bf3c81324615f0cb2ec976cfe
Robert Garcia <robbie559@gmail.com>
2021-03-17 00:58:49 -0700

    saving

---
+++ week-3/Final Project/actions.js
@@ -0,0 +1,20 @@
+console.log("JavaScript is working, PRAISE GOD!")
+;
+// function list
+function callAnger() {
+    var title=document.getElementById('container');
+    title.innerText = "Be ye angry, and sin not: let not the sun go down upon your wrath: -Paul's letter to the Hebrews 4:16";
+    
+}
+function callAnxiety() {
+    var title=document.getElementById('container')
+    title.innerText = "Be careful for nothing; but in every thing by prayer and supplication with thanksgiving let your requests be made known unto God.  -Paul's letter the Phillippians 4:6"
+}
+
+
+// event: click the button with your emotion
+
+// grab a preset message
+// display preset message
+// event: click to close the pop-up
+// repeat for 9 total buttons
